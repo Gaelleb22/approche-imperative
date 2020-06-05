abffe924679f17efe0775e65c3559e9656d2f4d4
Gaëlle BROSSAS <gaellebrossas@orange.fr>
2020-06-05 15:21:10 +0200

    3ème commit : après DEBUG

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -3,8 +3,24 @@ package fr.declaration.variable;
 public class DeclarationApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		byte a = 50;
+		short b = -12300;
+		int c = 2000000;
+		long d = 10000000L;
+		float e = 1.65F;
+		double f = 5.9;
+		char g = 'j';
+		boolean h = true;
+		String i ="Bonjour !";
+		
+		System.out.println(f);
+		
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		System.out.println(randomString);
+		String randomString1 = "Voici le résultat d'un calcul :";
+		String randomString2 = "1+5=6";
+		System.out.println(randomString1);
+		System.out.println(randomString2);
 	}
 
 }
